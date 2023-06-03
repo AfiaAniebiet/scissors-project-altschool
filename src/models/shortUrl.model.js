af937fb929dc48ccc61b4898b5004fb1ca90fb4d
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-03 15:11:38 +0100

    short url schema

---
+++ src/models/shortUrl.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema(
+  {
+    longUrl: {
+      type: String,
+      required: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+    },
+    customUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('ShortUrl', urlSchema);
