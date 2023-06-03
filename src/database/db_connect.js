3cd18c5d33a6d7c7b7fc9d3d2f79b8cd0d4a7b88
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-03 15:10:29 +0100

    connecting to database

---
+++ src/database/db_connect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('../config/default');
+
+const mongoDB_connection = async function () {
+  mongoose.set('strictQuery', false);
+
+  const MONGO_URI = config.MONGO_URI;
+
+  await mongoose
+    .connect(MONGO_URI)
+    .then((result) => {
+      console.log('Connection to mongoDB established successfully.');
+    })
+    .catch((error) => {
+      console.log('Failed to connect to database.');
+      console.log(error);
+    });
+};
+
+module.exports = mongoDB_connection;
