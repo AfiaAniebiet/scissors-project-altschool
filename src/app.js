b97e40ec4823dbc8f71f0b9c9798f52f3fc10f38
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-03 15:12:50 +0100

    express modules

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const cors = require('cors');
+require('express-async-errors');
+
+const shortUrlRoute = require('./routes/shortUrl.route');
+
+const app = express();
+
+// setting up ejs
+app.set('views', 'ejs');
+app.set('views', 'views');
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './', 'public')));
+
+app.use(shortUrlRoute);
+
+module.exports = app;
