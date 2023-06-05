96f381d0a872e43e80fb4bac16576c79c3131257
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-05 19:49:45 +0100

    controller functions

--- src/controllers/shortUrl.controller.js
+++ src/controllers/shortUrl.controller.js
@@ -1,13 +1,45 @@
 const ShortUrl = require('../models/shortUrl.model');
+const shortID = require('shortid');
+const validUrl = require('valid-url');
+const config = require('config');
+const { StatusCodes } = require('http-status-codes');
 
 const getIndexPage = function (req, res) {
   res.send('Index Page');
 };
 
-const makeShortUrl = (req, res) => {
-  const longUrl = req.body;
-  if (!longUrl) {
-    throw new Error({ msg: 'Enter a valid url to shorten' });
+const makeShortUrl = async (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get('baseUrl');
+
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(StatusCodes.UNAUTHORIZED).json('Invalid url');
+  }
+
+  // creating the url code
+  const urlCode = shortID.generate();
+
+  // check the long url from req.body
+  if (validUrl.isUri(longUrl)) {
+    let url = await ShortUrl.findOne({ longUrl });
+
+    if (url) {
+      res.json(url);
+    } else {
+      const shortUrl = `${baseUrl}/${urlCode}`;
+
+      url = new ShortUrl({
+        longUrl,
+        urlCode,
+        shortUrl,
+      });
+
+      await url.save();
+
+      res.status(StatusCodes.OK).json(url);
+    }
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).json({ error: 'Long url does not exist.' });
   }
 };
 
