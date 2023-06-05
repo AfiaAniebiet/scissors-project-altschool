442d145d86badd01e537ce9dde95b29f5600b152
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-05 19:52:23 +0100

    server setup

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const app = require('./app');
-const config = require('./config/default');
+const config = require('config');
 
 const mongoDB_connection = require('./database/db_connect');
 
-const PORT = config.PORT;
+const PORT = config.get('PORT');
 
+// creating the server using the app object
 const server = http.createServer(app);
 
+// connect to database and start server
 function startServer() {
   mongoDB_connection();
   server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
