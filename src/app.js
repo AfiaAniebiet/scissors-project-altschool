8df3f4bdec009c2dd5e9a7c71354b03722dd84fa
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-17 20:34:28 +0100

    setting up ejs

--- src/app.js
+++ src/app.js
@@ -2,23 +2,45 @@ const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('express-async-errors');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const shortUrlRoute = require('./routes/shortUrl.route');
 
-const app = express();
+// error Handling
+const errorHandlerMiddleware = require('./middlewares/errorHandler.middleware');
+const notFoundMiddleware = require('./middlewares/not-found');
 
-// setting up ejs
-app.set('views', 'ejs');
-app.set('views', 'views');
+const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setting up ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    page_title: 'Scissors App',
+  });
+});
 
 // Define routes
+app.use(limiter);
 app.use(shortUrlRoute);
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 module.exports = app;
