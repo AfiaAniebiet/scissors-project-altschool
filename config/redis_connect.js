817865523ebb2e0a348c17c84aa939cdb9ee88e0
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-22 19:48:16 +0100

    redis connection

--- config/redis_connect.js
+++ config/redis_connect.js
@@ -1,12 +1,17 @@
 const Redis = require('redis');
+const config = require('config');
 
 class Cache {
   constructor() {
     this.redis = null;
   }
 
-  connect() {
-    this.redis = Redis.createClient();
+  async connect() {
+    this.redis = await Redis.createClient({
+      url: `redis://${config.get('REDIS_USERNAME')}:${config.get('REDIS_PASSWORD')}@${config.get(
+        'REDIS_HOST'
+      )}:${config.get('REDIS_PORT')}`,
+    });
 
     this.redis.connect();
 
@@ -22,3 +27,21 @@ class Cache {
 
 const instance = new Cache();
 module.exports = instance;
+
+// const { createClient } = require('redis');
+// const redis = require('redis');
+
+// // const client = createClient();
+// const client = redis.createClient();
+
+// const redisConnection = function () {
+//   client.on('error', (err) => {
+//     console.log("Oops! Couldn't connect to Redis. Try again!", err);
+//   });
+
+//   client.on('connect', () => {
+//     console.log('Redis connection is established.');
+//   });
+// };
+
+// module.exports = { redisConnection, client };
