8dac73eac4ed5cd3fc442b8bb7f7bb3dde8b3eeb
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-22 19:46:51 +0100

    short url route

--- src/routes/shortUrl.route.js
+++ src/routes/shortUrl.route.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getIndexPage,
+  getRandomURLPage,
+  getCustomURLPage,
   makeShortUrl,
   shortUrlRedirect,
   generateCustomUrl,
 } = require('../controllers/shortUrl.controller');
 
-router.get('/short-url', getIndexPage);
+router.get('/random-url', getRandomURLPage);
+router.get('/custom-url', getCustomURLPage);
 router.post('/short-url', makeShortUrl);
 router.post('/custom-url', generateCustomUrl);
 router.get('/:code', shortUrlRedirect);
