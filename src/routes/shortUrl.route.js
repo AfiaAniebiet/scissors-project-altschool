5328d0a0ba9f02b8be5e8b10c66833aa9cf50ab3
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-25 16:53:40 +0100

    adding rate limiter

--- src/routes/shortUrl.route.js
+++ src/routes/shortUrl.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const apiLimiter = require('../middlewares/utils');
+
 const {
   getIndexPage,
   getRandomURLPage,
@@ -11,10 +13,10 @@ const {
 } = require('../controllers/shortUrl.controller');
 
 router.get('/', getIndexPage);
-router.get('/random-url', getRandomURLPage);
-router.get('/custom-url', getCustomURLPage);
-router.post('/short-url', makeShortUrl);
-router.post('/custom-url', generateCustomUrl);
+router.get('/random-url', apiLimiter, getRandomURLPage);
+router.get('/custom-url', apiLimiter, getCustomURLPage);
+router.post('/short-url', apiLimiter, makeShortUrl);
+router.post('/custom-url', apiLimiter, generateCustomUrl);
 router.get('/:code', shortUrlRedirect);
 
 module.exports = router;
