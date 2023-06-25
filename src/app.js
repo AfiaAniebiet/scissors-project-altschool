e5e1ea57c44e1b407be84bdf40ab2297d6b03a6e
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-25 16:36:06 +0100

    app object

--- src/app.js
+++ src/app.js
@@ -30,12 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    page_title: 'Scissors App',
-  });
-});
-
 // Define routes
 app.use(limiter);
 app.use(shortUrlRoute);
